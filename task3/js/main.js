50ce2b1225dee7d60aa88e8e268de2fd7df49ec4
doker_123 <dodo2larin@gmail.com>
2026-02-16 14:30:51 +0700

    Practical job 3 complete.

---
+++ task3/js/main.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
+    }
+})
