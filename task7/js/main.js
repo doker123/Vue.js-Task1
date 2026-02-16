ab441f6bca6baae1176a6c7f857e56ead43f3404
doker_123 <dodo2larin@gmail.com>
2026-02-16 16:11:44 +0700

    Practical job 7 complete.

---
+++ task7/js/main.js
@@ -0,0 +1,56 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        altText: "A pair of socks",
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        onSale: true,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            let saleString = ""
+          if (this.onSale) {
+              saleString = "live"
+          }
+          else {
+              saleString = "closed"
+          }
+          return `${this.brand} and their ${this.product} sale is ${saleString}`;
+        }
+    }
+})
