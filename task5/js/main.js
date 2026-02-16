23a0dd28539b433c35290e7845b47ef5420f878d
doker_123 <dodo2larin@gmail.com>
2026-02-16 15:02:59 +0700

    Practical job 5 complete.

---
+++ task5/js/main.js
@@ -0,0 +1,37 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: "2234",
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: "2235",
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            this.cart -= 1;
+            if (this.cart <= 0) {
+                this.cart = 0;
+            }
+        },
+        updateProduct(variantImage) {
+            this.image =  variantImage
+        }
+    }
+})
